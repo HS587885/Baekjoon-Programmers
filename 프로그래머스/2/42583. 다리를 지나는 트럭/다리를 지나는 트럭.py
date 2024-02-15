bf35770a42cd06bfb8713d58315a82d1cdc5cbd5
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-02-15 22:01:13 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,77 @@
+# from collections import deque
+
+# def solution(bridge_length, weight, truck_weights):
+#     second = 0
+#     current_weight = 0
+#     bridge = deque()
+
+#     while bridge or truck_weights:
+#         second += 1
+
+#         if bridge and second - bridge[0][1] == bridge_length:
+#             current_weight -= bridge.popleft()[0]
+
+#         if truck_weights and current_weight + truck_weights[0] <= weight:
+#             truck = truck_weights.pop(0)
+#             bridge.append((truck, second))
+#             current_weight += truck
+
+#     return second
+
+import collections
+
+DUMMY_TRUCK = 0
+
+
+class Bridge(object):
+
+    def __init__(self, length, weight):
+        self._max_length = length
+        self._max_weight = weight
+        self._queue = collections.deque()
+        self._current_weight = 0
+
+    def push(self, truck):
+        next_weight = self._current_weight + truck
+        if next_weight <= self._max_weight and len(self._queue) < self._max_length:
+            self._queue.append(truck)
+            self._current_weight = next_weight
+            return True
+        else:
+            return False
+
+    def pop(self):
+        item = self._queue.popleft()
+        self._current_weight -= item
+        return item
+
+    def __len__(self):
+        return len(self._queue)
+
+    def __repr__(self):
+        return 'Bridge({}/{} : [{}])'.format(self._current_weight, self._max_weight, list(self._queue))
+
+
+def solution(bridge_length, weight, truck_weights):
+    bridge = Bridge(bridge_length, weight)
+    trucks = collections.deque(w for w in truck_weights)
+
+    for _ in range(bridge_length):
+        bridge.push(DUMMY_TRUCK)
+
+    count = 0
+    while trucks:
+        bridge.pop()
+
+        if bridge.push(trucks[0]):
+            trucks.popleft()
+        else:
+            bridge.push(DUMMY_TRUCK)
+
+        count += 1
+
+    while bridge:
+        bridge.pop()
+        count += 1
+
+    return count
