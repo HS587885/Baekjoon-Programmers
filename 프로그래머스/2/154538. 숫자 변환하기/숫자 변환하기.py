a56671a560700a5e135ac5f718e852cb96f4b24e
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-02-18 21:48:07 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.96 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(x, y, n):
+    if x == y:
+        return 0
+
+    visited = set()  
+    queue = deque([(x, 0)])  
+
+    while queue:
+        current, count = queue.popleft()
+
+        if current == y:  
+            return count
+
+        
+        for operation in [current + n, current * 2, current * 3]:
+            if operation not in visited and operation <= y:  
+                visited.add(operation)
+                queue.append((operation, count + 1))
+
+    return -1  
