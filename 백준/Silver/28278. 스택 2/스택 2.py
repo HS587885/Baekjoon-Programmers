587d5a68a546af87cb894342055436f7b5ca4af9
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-04-07 16:22:22 +0900

    [Silver IV] Title: 스택 2, Time: 1288 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,26 @@
+import sys
+
+T = int(sys.stdin.readline().rstrip())
+
+stack = []
+for _ in range(T):
+    command = list(map(int, sys.stdin.readline().rstrip().split()))
+    if command[0] == 1:
+        stack.append(command[1])
+    elif command[0] == 2:
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == 3:
+        print(len(stack))
+    elif command[0] == 4:
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 5:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
