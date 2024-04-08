41c0f43aadb979a95ac1519b4e7d9287641d7afb
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-04-08 16:00:36 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,12 @@
+def solution(n, lost, reserve):
+    reserve_d = set(reserve) - set(lost)
+    
+    lost_d = set(lost) - set(reserve)
+    
+    for i in reserve_d:
+        if (i - 1) in lost_d:
+            lost_d.remove(i - 1)
+        elif (i + 1) in lost_d:
+            lost_d.remove(i + 1)
+            
+    return n - len(lost_d)
