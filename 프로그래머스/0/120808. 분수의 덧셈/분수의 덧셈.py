1f029838b9824d002c03277e5684a270db0dd332
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-05-09 13:42:49 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,6 @@
+from fractions import Fraction
+def solution(numer1, denom1, numer2, denom2):
+    answer = Fraction(numer1, denom1) + Fraction(numer2 , denom2)
+    return [answer.numerator, answer.denominator]
+        
+    
