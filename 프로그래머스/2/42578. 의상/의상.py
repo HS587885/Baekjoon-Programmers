9a9fadfaa79da738c348824326401a9f6698b7bd
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-07-05 14:20:26 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,16 +1,13 @@
+from collections import defaultdict
+
 def solution(clothes):
-    clothes_dict = {}
+    category_count = defaultdict(int)
+    for _, category in clothes:
+        category_count[category] += 1
+        
+    combinations = 1
+    for count in category_count.values():
+        combinations *= (count + 1)  
     
-   
-    for item, type in clothes:
-        if type not in clothes_dict:
-            clothes_dict[type] = 2  
-        else:
-            clothes_dict[type] += 1
     
-    
-    answer = 1
-    for key in clothes_dict:
-        answer *= clothes_dict[key]
-    
-    return answer - 1
+    return combinations - 1
