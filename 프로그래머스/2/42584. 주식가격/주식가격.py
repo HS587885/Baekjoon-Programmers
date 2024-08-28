362897ea8ec9688b3707c66bfca91ad1d9351395
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-08-28 23:00:53 +0900

    [level 2] Title: 주식가격, Time: 128.33 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,18 @@
 def solution(prices):
-    answer = [0] * len(prices)
-    stack = []
-    for i in range(len(prices)):
-        while stack and prices[stack[-1]] > prices[i]:
-            j = stack.pop()
-            answer[j] = i - j
-        stack.append(i)
-    while stack:
-        j = stack.pop()
-        answer[j] = len(prices) - j - 1
-    return answer
-            
-                    
+    result = []
     
+    for i in range(len(prices)):
+        time_point = prices[i]
+        time = 0
+        
+        for j in range(i + 1, len(prices)):
+            time += 1
+            if time_point > prices[j]:
+                break
+                
+        result.append(time)
+    
+    return result
+
+            
+            
