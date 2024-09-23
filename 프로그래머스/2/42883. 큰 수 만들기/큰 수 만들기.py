e3e4259cbb8224515a5ef77c0aeecd971b51562c
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-09-24 00:35:04 +0900

    [level 2] Title: 큰 수 만들기, Time: 82.37 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,15 +1,8 @@
 def solution(number, k):
     stack = []
-
     for num in number:
-        while stack and k > 0 and stack[-1] < num:
+        while k > 0 and stack and stack[-1] < num:
             stack.pop()
             k -= 1
         stack.append(num)
-
-    while k > 0:
-        stack.pop()
-        k -= 1
-
-    answer = ''.join(stack)
-    return answer
+    return ''.join(stack[:len(stack) - k])
