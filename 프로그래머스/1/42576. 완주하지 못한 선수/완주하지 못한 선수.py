4e6966dc220e35e35361065f83ad42e1604a3f58
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-10-01 14:12:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.70 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,3 @@
-from collections import Counter
-
 def solution(participant, completion):
-    p = Counter(participant)
-    c = Counter(completion)
-    result = "".join([i for i in p if p[i] > c[i]])
-    return  result
+    answer = ''
+    return answer
