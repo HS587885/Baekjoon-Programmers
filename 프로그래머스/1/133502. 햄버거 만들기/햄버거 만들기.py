d25af8ebcf4d0721718b4b9072f8576e38413884
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2024-10-06 17:47:54 +0900

    [level 1] Title: 햄버거 만들기, Time: 216.18 ms, Memory: 23.1 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,12 +1,9 @@
 def solution(ingredient):
-    answer = 0
-    
-    stack=[]
+    stack = []
+    cnt = 0
     for i in ingredient:
         stack.append(i)
-        if stack[-4:]==[1,2,3,1]:
-            answer+=1
-            for _ in range(4):
-                stack.pop()
-            
-    return answer
+        if stack[-4:] == [1,2,3,1]:
+            cnt += 1
+            del stack[-4:]
+    return cnt
