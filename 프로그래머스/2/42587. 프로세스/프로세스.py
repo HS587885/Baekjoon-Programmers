59471f29689a73ef8cbc99b5d8ae5358d33d36cf
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-02-07 21:26:53 +0900

    [level 2] Title: 프로세스, Time: 0.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,15 +1,15 @@
 from collections import deque
 
-def solution(priorities, location):
-    q = deque([(priority, idx) for idx, priority in enumerate(priorities)])
-    execution_order = 0
 
+def solution(priorities, location):
+    q = deque([pri, idx] for idx, pri in enumerate(priorities))
+    execution_order = 0
     while q:
         current = q.popleft()
-
+        
         if any(current[0] < item[0] for item in q):
             q.append(current)
         else:
-            execution_order += 1
+            execution_order +=1
             if current[1] == location:
                 return execution_order
