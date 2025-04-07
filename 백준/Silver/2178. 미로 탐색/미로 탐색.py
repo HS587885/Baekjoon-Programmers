a464718c5d6dc00c306034aec87a748b89382a1f
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-04-08 00:32:11 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 35076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,43 +1,26 @@
 from collections import deque
 
-# N, M을 공백으로 구분해서 입력 받기
 n, m = map(int, input().split())
+maps = [list(map(int, input().strip())) for _ in range(n)]  
 
-# 2차원 리스트의 맵 정보 입력 받기
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-# 이동할 네 방향 정의(상, 하, 좌, 우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-# BFS 소스코드 구현
-def bfs(x, y):
-    # 큐 구현을 위해 deque 라이브러리 사용
-    q = deque()
-    q.append((x, y))
+def solutions(maps, n, m):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     
-    # 큐가 빌 때까지 반복
+    q = deque([(0, 0)])
+    
     while q:
         x, y = q.popleft()
-        # 현재 위치에서 네 방향으로의 위치 확인
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            # 벽인 경우 무시
-            if graph[nx][ny] == 0:
-                continue
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                q.append((nx, ny))
+            
+            if 0 <= nx < n and 0 <= ny < m:
+                if maps[nx][ny] == 1:  
+                    maps[nx][ny] = maps[x][y] + 1 
+                    q.append((nx, ny))
     
-    # 가장 오른쪽 아래까지의 최단 거리 반환
-    return graph[n-1][m-1]
+    return maps[n-1][m-1] 
 
-# 최단 거리 출력
-print(bfs(0, 0))
+print(solutions(maps, n, m))
