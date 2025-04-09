fc04c2ae61f727bf127c755db9dda74cf5916e13
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-04-09 17:34:42 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 114144 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,26 @@
 from collections import deque
+import sys
 
-n, m = map(int, input().split())
-maps = [list(map(int, input().strip())) for _ in range(n)]  
+n,m = map(int, sys.stdin.readline().split())
+maps = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
 
-def solutions(maps, n, m):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+def solutions(maps):
     
-    q = deque([(0, 0)])
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     
-    while q:
-        x, y = q.popleft()
+    def bfs(x,y):
+        q = deque([(x,y)])
         
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < m:
-                if maps[nx][ny] == 1:  
-                    maps[nx][ny] = maps[x][y] + 1 
-                    q.append((nx, ny))
-    
-    return maps[n-1][m-1] 
+        while q:
+            x,y = q.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    q.append((nx,ny))
+        return maps[n-1][m-1]
+    return bfs(0,0)
 
-print(solutions(maps, n, m))
+print(solutions(maps))
