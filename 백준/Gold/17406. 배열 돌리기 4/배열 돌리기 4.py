8be0d9ef82ed6a4db34c8c88d275a8294ff2dfd0
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-14 23:06:55 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 1240 ms, Memory: 131924 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
@@ -1,6 +1,5 @@
 import sys
 import copy
-from itertools import permutations
 
 def rotate(arr, op):
     r, c, s = op
@@ -32,14 +31,34 @@ def rotate(arr, op):
 def get_min_row_sum(arr):
     return min([sum(row) for row in arr])
 
+def permutations(n, arr, lst = None, used =None, result =None):
+    if result is None:
+        result = []
+    if lst is None:
+        lst = []
+    if used == None:
+        used = [False] * len(arr)
+    if len(lst) == n:
+        result.append(lst)
+        return
+    for i in range(len(arr)):
+        if not used[i]:
+            used[i] = True
+            permutations(n, arr, lst + [arr[i]], used, result)
+            used[i] = False
+    return result
+
+
 # 입력
 N, M, K = map(int, sys.stdin.readline().split())
 A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 rotations = [tuple(map(int, sys.stdin.readline().split())) for _ in range(K)]
 
+
+
 # 가능한 모든 연산 순서에 대해 최소값 갱신
 answer = float('inf')
-for order in permutations(rotations, K):
+for order in permutations(K, rotations):
     temp = copy.deepcopy(A)
     for op in order:
         temp = rotate(temp, op)
