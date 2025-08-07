31fc310745c13732bda227fc3f71a1eb888dd3ed
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-08-07 23:52:24 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 114304 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -4,23 +4,26 @@ import sys
 n,m = map(int, sys.stdin.readline().split())
 maps = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
 
-def solutions(maps):
+def bfs(x, y):
+    q = deque([(x, y)])
     
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+    visited = [[0] * m for _ in range(n)]  
+    visited[x][y] = 1 
     
-    def bfs(x,y):
-        q = deque([(x,y)])
-        
-        while q:
-            x,y = q.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if maps[nx][ny] == 1 and visited[nx][ny] == 0:
                     maps[nx][ny] = maps[x][y] + 1
-                    q.append((nx,ny))
-        return maps[n-1][m-1]
-    return bfs(0,0)
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+    
+    return maps[n-1][m-1]
 
-print(solutions(maps))
+print(bfs(0, 0))
