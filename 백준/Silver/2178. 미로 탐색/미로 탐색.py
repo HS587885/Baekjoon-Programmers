8fda813dddd44ac8ba9fde28435e245e6a37e48a
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-08-09 00:20:00 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,26 @@
 from collections import deque
 import sys
 
+
 n,m = map(int, sys.stdin.readline().split())
+#print(n,m)
 maps = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
 
-def bfs(x, y):
-    q = deque([(x, y)])
+
+def bfs(x,y):
     
-    visited = [[0] * m for _ in range(n)]  
-    visited[x][y] = 1 
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
+    q = deque([(x,y)])
     while q:
-        x, y = q.popleft()
+        x,y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if maps[nx][ny] == 1 and visited[nx][ny] == 0:
-                    maps[nx][ny] = maps[x][y] + 1
-                    visited[nx][ny] = 1
-                    q.append((nx, ny))
-    
+            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                q.append((nx,ny))
     return maps[n-1][m-1]
 
-print(bfs(0, 0))
+print(bfs(0,0))
