3241d125554dc1912a33f48d4eb4c2edc4fb11f8
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-08-09 22:03:59 +0900

    [level 2] Title: 타겟 넘버, Time: 1118.23 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.7 MB, 시간: 4.34 ms
+메모리: 99 MB, 시간: 1118.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 21일 23:02:06
+2025년 08월 09일 22:03:57
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,16 @@
+from collections import deque
+
+
 def solution(numbers, target):
+    q = deque([(0,0)])
     answer = 0
+    
+    while q:
+        idx, total = q.popleft()
+        if idx == len(numbers):
+            if total == target:
+                answer += 1
+        else:
+            q.append((idx+1, total + numbers[idx]))
+            q.append((idx+1, total - numbers[idx]))
     return answer
