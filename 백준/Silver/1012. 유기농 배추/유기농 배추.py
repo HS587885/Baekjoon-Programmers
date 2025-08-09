b695af0d9e2646275beeac67bc4c85c3bb3bafd5
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-08-09 23:08:49 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 114904 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,44 +1,59 @@
 from collections import deque
 import sys
 
-def solution(maps, m, n):
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    visited = [[False] * m for _ in range(n)]
+input = sys.stdin.readline
 
+
+def solution(m,n,k, locations):
     
+    maps = [[0] * m for _ in range(n)]
+
+
+    for loc in locations:
+        ly,lx = loc[0],loc[1]
+        maps[lx][ly] = 1
+
+
+    visited = [[0] * m for _ in range(n)]
+
     def bfs(x,y):
+        
         q = deque([(x,y)])
+        
+        dx = [-1,1,0,0]
+        dy = [0,0,-1,1]
+        
         visited[x][y] = True
-        cnt = 1
+        
         while q:
-            x, y = q.popleft()
+            x,y = q.popleft()
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == False and maps[nx][ny] == 1:
-                    maps[nx][ny] = maps[x][y] + 1
-                    q.append((nx, ny))
-                    cnt += 1
-        return cnt
+                if 0 <= nx < n and 0 <= ny < m:
+                    if maps[nx][ny] == 1 and visited[nx][ny] == 0:
+                        maps[nx][ny] = maps[x][y] + 1
+                        q.append((nx,ny))
+                        visited[nx][ny] = 1
+                    
         
-    max_cnt = []
+
+
+    count = 0
     for i in range(n):
         for j in range(m):
-            if maps[i][j] == 1 and not visited[i][j]:
-                max_cnt.append(bfs(i,j))
-    return len(max_cnt)
+            if maps[i][j] == 1 and visited[i][j] == 0:
+                bfs(i,j)
+                count += 1
 
 
-
-T = int(sys.stdin.readline())
-
-for _ in range(T):
-    m,n,k = map(int, sys.stdin.readline().split())
-    cabbage_location  = [list(map(int, sys.stdin.readline().split())) for _ in range(k)]
+    return count   
     
-    maps = [[0 for _ in range(m)] for _ in range(n)]
-    for i in cabbage_location:
-        maps[i[1]][i[0]] = 1
 
-    print(solution(maps, m, n))
+T = int(input())
+for _ in range(T):
+    m,n,k = map(int, input().split())
+    locations = [list(map(int, input().split())) for _ in range(k)]
+    print(solution(m,n,k, locations))
+
+ 
